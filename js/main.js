d62b1c0cd7d47a573a04c648fd403f1f0b4916ab
mpenyanaryan <mpenyanaryan@gmail.com>
2022-10-10 10:29:53 -0700

    uploading websitd

---
+++ js/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
+var collapseList = collapseElementList.map(function (collapseEl) {
+    return new bootstrap.Collapse(collapseEl)
+})
